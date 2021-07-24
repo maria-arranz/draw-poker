63eeb9220ae13d626dcfef8943343731c13d7269
mariaal <mariaarranzl@gmail.com>
2021-07-24 20:55:16 +0200

    clasificador

--- draw-poker.py
+++ draw-poker.py
@@ -53,3 +53,44 @@ def descendente(lista):
 #Comprueba si es el caso especial de una escalera donde A es la carta mas baja en una lista ordenada de cartas
 def especial_flush(lista):
     return lista == ["A","5","4","3","2"]
+
+
+#una mano será un set: esta funcion devuelve de cual se trata
+def clasifica(mano):
+    #como mano inmutable lo pasamos a lista
+    s = list(mano)
+    #a continuacion ordenamos la lista para tener una manera uniforme de detectar el tipo de mano
+    ordena(s)
+    valores = list(map(lambda x: x[0], s))
+    palos = list(map(lambda x: x[1], s))
+    color = mismo_color(palos)
+    seguidas = descendente(valores)
+    apariciones = list(map (lambda x: valores.count(x), values))
+    maximo = max(apariciones)
+    
+    if (seguidas and color):
+        if (valores[0] == "A"):
+            return (1,"A")
+        else:
+            return (2,valores[0])
+    
+    if (color):
+        if (especial_flush(valores)): return (2,"5")
+        else: return (5,valores[0])
+    if (maximo == 4):
+        return (3,values[apariciones.index(4)])
+    if(maximo == 3) and (2 in apariciones):
+        return (4, values[apariciones.index(3)])
+    if(seguidas):
+        return (6, valores[0])
+    if(especial_flush(valores)):
+        return (6, "5")
+    if(maximo == 3):
+        return (7,values[apariciones.index(3)])
+    if(apariciones.count(2) == 2):
+        return (8,values[apariciones.index(2)]) #como estan ordenados, la primera aparicion tiene mayor valor
+    if (2 in apariciones):
+        return (9,values[apariciones.index(2)])
+    else :return (10,valores[0])
+
+# En conclusion se devuelve el tipo de mano y el primer criterio de desempate
